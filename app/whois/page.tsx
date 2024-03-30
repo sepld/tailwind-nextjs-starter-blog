2cf1c66ef556e4f03bf5eb8dce0ef4852154f255
sepld <li_dong01@outlook.com>
2024-03-30 11:19:52 +0800

    Update page.tsx

--- app/whois/page.tsx
+++ app/whois/page.tsx
@@ -1,7 +1,7 @@
 import { genPageMetadata } from 'app/seo'
 import WhoisService from '@/components/WhoisService'
 
-export const metadata = genPageMetadata({ title: 'Whois', description: 'name servcies'} )
+export const metadata = genPageMetadata({ title: 'Whois', description: 'name servcies' })
 
 export default function Page() {
   return (
