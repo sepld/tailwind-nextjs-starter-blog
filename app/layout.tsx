072a2c92d4d51674016fbf82f252c60472a9c9dc
sepld <li_dong01@outlook.com>
2024-03-31 13:19:27 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -75,6 +75,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
       <body className="bg-white text-black antialiased dark:bg-gray-950 dark:text-white">
         <ThemeProviders>
+          <VercelAnalytics />
           <Analytics analyticsConfig={siteMetadata.analytics as AnalyticsConfig} />
           <SectionContainer>
             <div className="flex h-screen flex-col justify-between font-sans">
