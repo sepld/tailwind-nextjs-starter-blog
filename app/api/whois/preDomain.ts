f4f4c4d14549f901197076d4e4235735eca65e54
sepld <li_dong01@outlook.com>
2024-04-01 13:24:39 +0800

    format

--- app/api/whois/preDomain.ts
+++ app/api/whois/preDomain.ts
@@ -3,45 +3,41 @@ import * as net from 'net'
 import * as punycode from 'punycode'
 
 export default function preDomain(domain: string) {
-    var parts, tld, server_tld, server_ip
-    var server = {
-        follow: 2,
-        timeout: 10000, // 60 seconds in ms
-        query: '$addr\r\n',
-        punycode: false,
-        domain: domain,
-        host: null,
-    }
+  let parts, tld, server_tld, server_ip
+  let server = {
+    follow: 2,
+    timeout: 10000, // 60 seconds in ms
+    query: '$addr\r\n',
+    punycode: false,
+    domain: domain,
+    host: null,
+  }
 
-    switch (true) {
-        case net.isIP(domain) !== 0:
-            server_ip = servers['_']['ip']
-            break
-        default:
-            tld = punycode.toASCII(domain)
-            while (true) {
-                server_tld = servers[tld]
-                if (!tld || server_tld) {
-                    break
-                }
-                tld = tld.replace(/^.+?(\.|$)/, '')
-            }
-    }
-    if (typeof server_tld === 'string') {
-        parts = server_tld.split(':')
-        server_tld = {
-            host: parts[0],
-            port: parts[1],
+  switch (true) {
+    case net.isIP(domain) !== 0:
+      server_ip = servers['_']['ip']
+      break
+    default:
+      tld = punycode.toASCII(domain)
+      while (true) {
+        server_tld = servers[tld]
+        if (!tld || server_tld) {
+          break
         }
+        tld = tld.replace(/^.+?(\.|$)/, '')
+      }
+  }
+  if (typeof server_tld === 'string') {
+    parts = server_tld.split(':')
+    server_tld = {
+      host: parts[0],
+      port: parts[1],
     }
+  }
 
-    if (server.punycode !== false) {
-        server.domain = punycode.toASCII(domain)
-    }
-    server = {
-        ...server,
-        ...server_ip,
-        ...server_tld,
-    }
-    return server
+  if (server.punycode !== false) {
+    server.domain = punycode.toASCII(domain)
+  }
+  Object.assign(server, server_ip, server_tld)
+  return server
 }
